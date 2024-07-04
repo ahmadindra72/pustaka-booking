ab0c292d6813b1aab952b7c9d362307ef1c0430a
ahmadindra72 <ahmadindra720@gmail.com>
2024-07-04 23:04:00 +0700

    tugas WP2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Ahmad Indra J
+            Saya tinggal di daerah Bekasi
+            Hobby saya adalah rebahan";
+    }
+}
